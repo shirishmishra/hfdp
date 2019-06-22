c206e696753fb4dec9cfb78bc95d97c61accfac2
Shirish Mishra <ershirish@yahoo.com>
2019-06-22 09:07:05 +0530

    Added code from chapter 3

---
+++ decorator/io/InputTest.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class InputTest {
+	public static void main(String[] args) throws IOException {
+		int c;
+		try {
+			InputStream in =
+				new LowerCaseInputStream(
+						new BufferedInputStream(
+							new FileInputStream("test.txt")));
+
+
+			while((c = in.read()) >= 0) {
+				System.out.print((char)c);
+			}
+
+			in.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
